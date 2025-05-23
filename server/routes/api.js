602b4d5d5886755a41c5ed46be1b453c44f62c94
Dmitriy Danilov <dima-danilov1995@gmail.com>
2025-05-23 14:25:04 +0300

    commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,27 +1,43 @@
 import axios from 'axios';
 
-export function apiRoutes (fastify, opts) {
+export function apiRoutes(fastify, opts) {
     // Middleware для проверки авторизации
     fastify.addHook('preHandler', (request, reply, done) => {
         if (!request.session.access_token) {
-            reply.code(401).send({ error: 'Unauthorized' });
+            reply.code(401).send({error: 'Unauthorized'});
             return;
         }
         done();
     });
 
     // Пример защищенного API
-    fastify.get('/data', async (request, reply) => {
+    fastify.get('*', async (request, reply) => {
         try {
             // Используем токен для запроса к внешнему API
-            const response = await axios.get('https://api.provider.com/data', {
+            const response = await axios.get(process.env.API_URL + request.params['*'], {
                 headers: {
-                    Authorization: `Bearer ${request.session.access_token}`
+                    Authorization: `Bearer ${request.session.access_token}`,
+                    ...request.headers,
                 }
             });
             reply.send(response.data);
         } catch (error) {
-            reply.code(500).send({ error: 'API request failed' });
+            reply.code(500).send({error: 'API request failed'});
+        }
+    });
+
+    fastify.post('*', async (request, reply) => {
+        try {
+            // Используем токен для запроса к внешнему API
+            const response = await axios.post(process.env.API_URL + request.params['*'], {
+                headers: {
+                    Authorization: `Bearer ${request.session.access_token}`,
+                    ...request.headers,
+                }
+            });
+            reply.send(response.data);
+        } catch (error) {
+            reply.code(500).send({error: 'API request failed'});
         }
     });
 }
