bea49e5e9c0148f8040ef037c511deac73b0d2f0
Dmitriy Danilov <dima-danilov1995@gmail.com>
2025-06-08 13:51:20 +0300

    Revert "commit"
    This reverts commit cf2397b03df08d7c2277efcedfca605d5b5f6bcf.

--- server/app.js
+++ server/app.js
@@ -1,18 +1,36 @@
+const Fastify = require('fastify');
 const fastifySession = require('@fastify/session');
 const fastifyCookie = require('@fastify/cookie');
 
 const { auth } = require('./routes/auth.js');
 const { apiRoutes } = require('./routes/api.js');
 
-module.exports = function (fastify, options, next) {
+const PORT = process.env.PORT ?? 3000;
+
+const fastify = Fastify({
+    logger: true
+});
+
+const init = () => {
+
+// Плагины для работы с сессиями и куками
     fastify.register(fastifyCookie);
     fastify.register(fastifySession, {
-        secret: process.env.SESSION_SECRET,
-        cookie: {secure: true} // Для HTTPS: secure: true
+        secret: process.env.SESSION_SECRET, // Замените на надежный ключ
+        cookie: {secure: false} // Для HTTPS: secure: true
     });
 
     fastify.register(auth, {prefix: '/auth'});
     fastify.register(apiRoutes, {prefix: '/api'});
 
-    next()
+    fastify.listen({port: PORT}, (err) => {
+        if (err) throw err;
+        console.log('Server running on http://localhost:' + PORT);
+    });
 }
+
+const close = () => fastify.close();
+
+exports.fastify = fastify;
+exports.init = init;
+exports.close = close;
